1f3c57aed727644d7c3a7606eeced4d4a187fe81
Adam Widby <adam@adam-desktop.local>
2023-03-27 10:38:18 -0700

    Exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -41,9 +41,9 @@ const Part = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-
-  const parts = [
+  const course = {
+    name:'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -57,12 +57,13 @@ const App = () => {
       exercises: 14
     }
   ]
+}
 
   return (
     <div>
-      <Header name={course} />
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
 
   )
